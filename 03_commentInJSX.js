fe0f79db7d9397147350369675e0afbac6aef1c8
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-12-19 18:33:42 +0200

    Add a comment with JSX syntax

--- 03_commentInJSX.js
+++ 03_commentInJSX.js
@@ -1,5 +1,6 @@
 const JSX = (
     <div>
+    {/* This is considered one element since the elements below are nested within the parent div element. */}
         <h1>This is a block of JSX</h1>
         <p>Here's a subtitle</p>
     </div>
