fc8561ac1df69ee361b086ff71d9cb237017dab8
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-12-20 20:44:22 +0200

    Fix the missing closing element syntax

--- 06_self-ClosingJSXTags.js
+++ 06_self-ClosingJSXTags.js
@@ -1,8 +1,8 @@
 const JSX = (
     <div>
         <h2>Welcome to React!</h2>
-        <br >
+        <br /> {/* All element must be closed in JSX AND can be closed like this - if they have no content */}
         <p>Be sure to close all tags!</p>
-        <hr >
+        <hr />
     </div>
 );
