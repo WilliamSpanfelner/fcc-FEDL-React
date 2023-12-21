d1d4eead7d31e6a98eaf18e566892eb2ea64bf17
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-12-21 22:18:34 +0200

    Create a component to benefit from added functionality

--- 08_createAReactComponent.js
+++ 08_createAReactComponent.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
 class MyComponent extends React.Component {
     constructor(props) {
         super(props);
     }
     render() {
-        
+        return (
+            <div>
+                <h1>Hello React!</h1>
+            </div>
+        );
     }
 };
 
