3dc21a35e3967f7f517d4fa873832e763f1f0919
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2023-12-30 22:54:57 +0200

    Starting code

---
+++ 12_renderAClassComponent2TheDOM.js
@@ -0,0 +1,13 @@
+class TypesOfFood extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <h1>Types of Food:</h1>
+
+            </div>
+        );
+    }
+};
