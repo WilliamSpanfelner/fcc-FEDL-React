2e8be6d111de2975447874d0aad50553d06d93db
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-01-04 20:50:08 +0200

    Starting code

---
+++ 22_renderStateInTheUI.js
@@ -0,0 +1,15 @@
+class MyComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: 'freeCodeCamp'
+        }
+    }
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+};
