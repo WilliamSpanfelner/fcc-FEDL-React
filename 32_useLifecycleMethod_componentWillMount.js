92b64b4326b924168c618e59f10c9019def1cd50
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-01-08 19:23:37 +0200

    Starting code

---
+++ 32_useLifecycleMethod_componentWillMount.js
@@ -0,0 +1,13 @@
+class MyComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    componentWillMount() {
+
+        
+
+    }
+    render() {
+        return <div />
+    }
+};
