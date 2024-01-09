d804999272ddf32db471aeb935cde262ea1af8d0
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-01-09 19:55:04 +0200

    Add a styles var and reference it to style div

--- 37_reactInlineStyles.js
+++ 37_reactInlineStyles.js
@@ -1,9 +1,15 @@
+const styles = {
+    color: 'purple',
+    fontSize: 40,
+    border: '2px solid purple'
+}
+
 class Colorful extends React.Component {
     render() {
 
 
         return(
-            <div style={{color: 'yellow', fontSize: 24}}>Style Me!</div>
+            <div style={styles}>Style Me!</div>
         );
 
     }
