c706613ef4a4883697d5627a81714cbfdc205bdb
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-01-10 20:57:54 +0200

    Starting code

---
+++ 39_renderWithIf-Else.js
@@ -0,0 +1,24 @@
+class MyComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dispaly: true
+        }
+        this.toggleDisplay = toggleDisplay.bind(this);
+    }
+    toggleDisplay() {
+        this.setState((state) => ({
+            display: !state.dispaly
+        }));
+    }
+    render() {
+
+        
+        return (
+            <div>
+                <button onClick={this.toggleDisplay}>Toggle Display</button>
+                <h1>Displayed!</h1>
+            </div>
+        );
+    }
+};
